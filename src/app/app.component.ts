6f04bf1e9ab0a6fdde16f164e86397ddaf543d6d
kaky <lachapelle.kaky@gmail.com>
2021-02-05 16:40:31 +0300

    firebase realtime database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AuthService } from './services/auth.service';
 
+import { AngularFireDatabase } from '@angular/fire/database';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -15,7 +18,8 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    public authService: AuthService
+    public authService: AuthService,
+    public afDB: AngularFireDatabase
   ) {
     this.initializeApp();
   }
@@ -25,5 +29,20 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  ngOnInit() {
+    this.add();
+  }
+
+
+  add() {
+    this.afDB.list('people').valueChanges().subscribe(tab => {
+      console.log(tab);
+    })
+    // console.log(this.afDB.list('people/'));
+    // this.afDB.list('people/').push({
+    //   name: 'jean'
+    // });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy, MenuController } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -11,10 +11,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomePageModule } from './home/home.module';
 import { AUTH_PROVIDERS } from './services/auth.service';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQNh1grr366oN7RC-BzhUELDMI-XnYnX0",
+  authDomain: "learn-jean.firebaseapp.com",
+  projectId: "learn-jean",
+  storageBucket: "learn-jean.appspot.com",
+  messagingSenderId: "1000413520391",
+  appId: "1:1000413520391:web:87531f67f8d54db67fc0a2",
+  measurementId: "G-GZGXFW9739"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule,  IonicModule.forRoot(), AppRoutingModule, HomePageModule],
+  imports: [BrowserModule, IonicModule, IonicModule.forRoot(), AppRoutingModule, HomePageModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
@@ -23,4 +43,4 @@ import { AUTH_PROVIDERS } from './services/auth.service';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
